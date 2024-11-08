52d9b45a53cff5ecf25a625fa6676fe2973d5977
GustavoTecnologia <00001094805701SP@al.educacao.sp.gov.br>
2024-11-08 12:07:16 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,22 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'qual e a capital do brasil ?
+)  a capital do brasil e brasilia
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
